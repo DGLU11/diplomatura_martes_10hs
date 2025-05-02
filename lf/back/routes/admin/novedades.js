527d27bb461a92c2c21fc4fd16c716d5ec5ddebc
lourdes fernandez rubio <dgfernandezrubio@gmail.com>
2025-05-02 17:11:54 -0300

    C:\Users\Windows\Desktop\diplomatura_martes_10hs\lfweb

--- lf/back/routes/admin/novedades.js
+++ lf/back/routes/admin/novedades.js
@@ -66,29 +66,39 @@ router.get('/agregar', (req, res, next) => {
     router.get('/modificar/:id', async (req, res, next) => {
       let id = req.params.id;
       let novedad = await novedadesModel.getNovedadById(id);
+
+      if (novedad.img_id) {
+        const imagen = cloudinary.image(novedad.img_id, {
+          width: 100,
+          height: 100,
+          crop: 'fill'
+        });
+        novedad.imagen = imagen;
+      } else {
+        novedad.imagen = '';
+      }
       res.render('admin/modificar', {
         layout: 'admin/layout',
         novedad
       });
     });
- 
-               
+     
 
       var destroy = util.promisify(cloudinary.uploader.destroy); 
     
-      router.post('/modificar', async (req, res, next) => {
-        try {
-          let img_id = req.body.img_original;
-          let borrar_img_vieja = false;
+               router.post('/modificar', async (req, res, next) => {
+            try {
+              let img_id = req.body.img_original;
+              let borrar_img_vieja = false;
 
-          if (req.body.img_delete === "1") {
-            img_id = null;
-            borrar_img_vieja = true;
-          } else if (req.files && Object.keys(req.files).length > 0) {
-            let imagen = req.files.imagen;
-            img_id = (await uploader(imagen.tempFilePath)).public_id;
-            borrar_img_vieja = true;
-          }
+              if (req.body.img_delete === "1") {
+                img_id = null;
+                borrar_img_vieja = true;
+              } else if (req.files && Object.keys(req.files).length > 0) {
+                let imagen = req.files.imagen;
+                img_id = (await uploader(imagen.tempFilePath)).public_id;
+                borrar_img_vieja = true;
+              }
 
           if (borrar_img_vieja && req.body.img_original) {
             await destroy(req.body.img_original);
@@ -120,7 +130,8 @@ router.get('/agregar', (req, res, next) => {
         lugar: req.body.lugar,
         fechadeinicio: req.body.fechadeinicio,
         tipodeobra: req.body.tipodeobra,
-        estado: req.body.estado
+        estado: req.body.estado,
+        img_id: req.body.img_original
       }
     });
   }
--- lf/front/src/pages/Contacto.js
+++ lf/front/src/pages/Contacto.js
@@ -1,43 +1,83 @@
 import "./../styles/contacto.css";
+import { useState } from 'react';
+import axios from 'axios';
+
 
 const Contacto = (props) => {
+    const initialForm = {
+        nombre: '',
+        email: '',
+        telefono: '',
+        lugar: '',
+        mensaje: ''
+    };
+
+    const [sending, setSending] = useState(false);
+const [msg, setMsg] = useState('');
+const [formData, setFormData] = useState(initialForm);
+
+const handleChange = e => {
+    const { name, value } = e.target;
+    setFormData(oldData => ({
+        ...oldData,
+        [name]: value
+    }));
+};
+    
+const handleSubmit = async e => {
+    e.preventDefault();
+    setMsg('');
+    setSending(true);
+    const response = await
+         axios.post('http://localhost:3000/api/contacto', formData);
+        setSending(false);
+        setMsg(response.data.message);
+        if (response.data.error === false) {
+            setFormData(initialForm);
+        }
+    }
+    
     return (
         <div>
             <h1>Contacto</h1>
             <main className="holder contacto">
                 <div>
                     <h2>Contacto</h2>
-                    <form action="procesar_formulario.php" method="POST" className="formulario">
+                    <form action="/contacto" method="post" onSubmit={handleSubmit} className="formulario">
                         
                         <p>
                             <label for="nombre">Nombre y Apellido</label>
-                            <input type="text" />
+                            <input type="text" name="nombre" value= {formData.nombre} onChange={handleChange}/>
                         </p>
                         
                         <p>
                             <label for="email">Email</label>
-                            <input type="text" />
+                            <input type="text" name="email" value= {formData.email} onChange={handleChange}/>
                         </p>
                         
                         <p>
                             <label for="telefono">Teléfono</label>
-                            <input type="text" />
+                            <input type="text" name="telefono" value={formData.telefono} onChange={handleChange} />
                         </p>
                         
                         <p>
                             <label for="lugar">Lugar de la Obra</label>
-                            <input type="text" />
+                            <input type="text" name="lugar" value={formData.lugar} onChange={handleChange} />
+
                         </p>
                         
                         <p>
                             <label for="mensaje">Mensaje</label>
-                            <textarea></textarea>
+                            <textarea name="mensaje" value= {formData.mensaje} onChange={handleChange}></textarea>
                         </p>
                         
                         <p>
                             <input type="submit" value="Enviar" />
                         </p>
                     </form>
+
+                   {sending ?  <p>Enviando...</p> : null}
+                   {msg ?  <p>{msg}</p> : null}
                 </div>
             </main>
         </div>
